2bdeb8ba80d48f3aa71fb4bc5fa6ad60de6f86f6
kaush <emailtokaush@gmail.com>
2020-07-04 15:16:21 -0400

    removed old project with defects

--- stock-analysis-2nd/src/aggregators/AggregatorProcessor.java
+++
@@ -1,6 +0,0 @@
-package aggregators;
-
-public class AggregatorProcessor {
-	
-	
-}
--- stock-analysis-2nd/src/client/Application.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-public class Application {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
