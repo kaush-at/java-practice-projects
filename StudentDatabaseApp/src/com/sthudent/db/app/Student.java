1089864dbc84bbaa1947a99f770f5a8bc7976cc0
kaush <emailtokaush@gmail.com>
2020-07-06 04:11:03 -0400

    add StudentDatabseApp project

---
+++ StudentDatabaseApp/src/com/sthudent/db/app/Student.java
@@ -0,0 +1,45 @@
+package com.sthudent.db.app;
+
+public class Student {
+
+	private String firstName;
+	private String lastName;
+	private int gradedYear;
+	private String studentId;
+	private int tutionBalance;
+	private int costOfCource = 600;
+	private String [] subjects;
+	private static int id = 1000;
+	//  static unama class ekata podui object ekakata neme (aluthen object ekak haduwat class ekata podu nisa class eke dan tiyena value eka tama ganne)
+	
+	public Student(String firstName, String lastName, int gradedYear, String [] subjects ) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gradedYear = gradedYear;
+		this.subjects = subjects;
+		
+		setStudentId();
+		setTutionBalance(this.subjects.length);
+		
+		System.out.println("Student name id "+firstName+" "+lastName+"\nStudent Id is "+studentId+"\nYour tution fee total is : "+tutionBalance);
+		System.out.println("You have enrolled to :" );
+		for(String subject : subjects) {
+			System.out.println("\t"+subject);
+		}
+	}
+	
+	private void setStudentId() {
+		id++;
+		this.studentId =  this.gradedYear +""+id;
+	}
+	
+	private void setTutionBalance(int subjectCount) {
+		this.tutionBalance = subjectCount * costOfCource;
+	}
+	
+	public int getBalance(int paidAmmount) {
+		return tutionBalance - paidAmmount;
+	}
+	
+	
+}
---
+++ StudentDatabaseApp/src/com/sthudent/db/app/StudentDbApplication.java
@@ -0,0 +1,60 @@
+package com.sthudent.db.app;
+
+import java.util.Scanner;
+
+public class StudentDbApplication {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("How many students you need to add ?");
+		int noOfStudents = sc.nextInt();
+		Student [] students = new Student[noOfStudents];
+		
+		String fName;
+		String lName;
+		int year;
+		int subjectCount;
+		
+		for(int i = 0; i < noOfStudents; i++) {
+			//Students[i] = new Student();
+			System.out.println("Enter the student first name : ");
+			fName = sc.next();
+			
+			System.out.println("Enter the student last name : ");
+			lName = sc.next();
+			
+			System.out.println("Enter the student class year \n1 for Freshman\n2 for Sophmore\n3 for Junior\n4 for Senior: ");
+			year = sc.nextInt();
+			
+			System.out.println("How many Subjects would you like to enroll ? ");
+			subjectCount = sc.nextInt();
+			String[] subjects = new String[subjectCount];
+			
+			int counter = 0;
+			while(counter < subjectCount) {
+				System.out.println("Please enter the Subject you gonna enroll");
+				String userInput;
+				String first = sc.next(); // Read past the text EOL character(s) -> skip the white line issue
+				String second = sc.nextLine();
+				userInput = first +" " + second;
+				//String userInput = sc.nextLine(); 
+				subjects[counter] = userInput;
+				counter++;
+				}
+			
+
+			students[i]  = new Student(fName, lName, year, subjects);
+			
+			System.out.println("How much would you like to pay initially :");
+			int paidAmount = sc.nextInt();
+			
+			int balanceToPay = students[i].getBalance(paidAmount);
+			System.out.println("After you pay $"+paidAmount+" Your remaining balance is "+balanceToPay);
+//			count--;
+		}
+		System.out.println();
+		System.out.println("Thanks for all and wish you GOOD LUCK..");
+
+	}
+
+}
